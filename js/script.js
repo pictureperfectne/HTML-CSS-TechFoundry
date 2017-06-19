f52ddfccc4d26600a8e53c739acf7eced13e699d
Mike Robinson <info@pictureperfectwebdesign.com>
2017-06-18 21:07:05 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function (){
+	/*
+ $(function(){
+		$("a").click(function(){
+			if(this.hash){
+			var hash = this.hash.substr(1);
+			var cp = $("a[name"+hash+"]").position();
+					
+			$("body,html").animate({
+			scrollTop: cp.top
+			
+			}, 2000,"easeOutExpo");
+
+
+			//scroll / animate to element
+
+			//don;t jump to link
+
+			return false;
+			}
+		});
+});
+   
+ /*$('a[href^="#"]').on('click', function(event) {
+
+    //var target = $(this.getAttribute('href'));
+	var target = $(this).attr('href');
+alert(target);
+    if(target.length) {
+	alert(target.length);
+        event.preventDefault();
+        //$('html, body').animate({scrollTop: $("#top").offset().top}, 2000);
+		$('html, body').animate({
+			scrollTop: $(target).offset().top}, 1000);
+    }
+
+});*/
+
+});
